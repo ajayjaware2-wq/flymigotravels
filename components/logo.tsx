b2b08f9bb2b499ecf53a2907278928ac5d01792f
ajayjaware2-wq <ajayjaware2@gmail.com>
2026-07-17 19:00:12 +0530

    logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -9,7 +9,7 @@ export function Logo({ className }: LogoProps) {
   return (
     <div className={`flex items-center ${className ?? ""}`}>
       <Image
-        src="/images/flymigo-logo.jpeg3.jpeg"
+        src="/images/flymigo-logo.jpeg3.jpeg.png"
         alt="Flymigo Travels"
         width={65}
         height={65}
